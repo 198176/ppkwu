7671d336698915010af63bbdf17e99cbc5a77124
198176 <adriantomczak36@gmail.com>
2018-04-15 10:08:02 +0200

    Added checkMail

---
+++ src/main/java/com/example/demo/FormController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FormController {
+
+    @RequestMapping("/checkMail")
+    @ResponseBody
+    public Mail checkMail(@RequestParam("mail") String mail) {
+        return new Mail(mail);
+    }
+}
